00b361e486616207959cc8e91572327ba862027b
Haotian Yu <yuhaotian1437@gmail.com>
2025-01-02 16:33:12 +0800

    增加按时间获取运行记录的功能

--- module/webServer/routes/get/get_var.py
+++ module/webServer/routes/get/get_var.py
@@ -1,5 +1,6 @@
 from aiohttp.web import UrlDispatcher, Request, HTTPOk, HTTPBadRequest
 from module.db.mysqlConn import MySqlConn
+from datetime import datetime
 import json
 
 def add_get(router:UrlDispatcher):
@@ -40,18 +41,50 @@ async def filter_var_id(request:Request):
 async def handle_device_var(request:Request):
     try:
         # 从 GET 请求中获取 'device_id' 参数
-        full_code = request.query.get('full_code')  # 从查询参数中获取字段 
-        
+        full_code = request.query.get('full_code')  # 从查询参数中获取字段
+        start_date_str = request.query.get('start_date')  # 获取开始时间
+        end_date_str = request.query.get('end_date')  # 获取结束时间
+        data_length = request.query.get('data_length')
+
         # 用下划线替换点号，避免表名错误
         table_name = f"var_{full_code.replace('.', '_')}"
 
-        vars = await MySqlConn.rawSqlCmd(f'''SELECT value, created_at from `{table_name}` ORDER BY id DESC LIMIT 20''')
+        # 构造 SQL 查询
+        query = f'''SELECT value, created_at FROM {table_name} WHERE 1=1'''
+
+        # 如果提供了开始时间，则添加时间范围过滤
+        if start_date_str:
+            try:
+                start_date = datetime.fromisoformat(start_date_str)  # 转换为 datetime 对象
+                query += f" AND created_at >= '{start_date.isoformat()}'"
+            except ValueError:
+                raise ValueError(f"Invalid start_date format: {start_date_str}")
+            
+        # 如果提供了结束时间，则添加时间范围过滤
+        if end_date_str:
+            try:
+                end_date = datetime.fromisoformat(end_date_str)  # 转换为 datetime 对象
+                query += f" AND created_at <= '{end_date.isoformat()}'"
+            except ValueError:
+                raise ValueError(f"Invalid end_date format: {end_date_str}")
+
+        if data_length:    
+            query += f" ORDER BY id DESC LIMIT {data_length}"
+        else:
+            query += f" ORDER BY id DESC LIMIT 20"
+        
+        # 执行查询
+        vars = await MySqlConn.rawSqlCmd(query)
+        
         # print(vars)
         var_list = [{"value": var[0], "created_at": var[1]} for var in vars]
         # print(var_list)
         # 转换 datetime 为字符串
         for item in var_list:
-            item['created_at'] = item['created_at'].isoformat()
+            if item['created_at'] is not None:
+                item['created_at'] = item['created_at'].isoformat()
+            else:
+                item['created_at'] = ""  # 或者设置为一个默认的日期字符串
         return HTTPOk(text=json.dumps(var_list))
     
     except ValueError as ve:
