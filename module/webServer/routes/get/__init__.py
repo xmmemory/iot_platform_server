d33e698cae5d3b0a30f0d93caa2be3badb11c194
Haotian Yu <yuhaotian1437@gmail.com>
2025-01-05 11:33:19 +0800

    sync with app.

--- module/webServer/routes/get/__init__.py
+++ module/webServer/routes/get/__init__.py
@@ -4,14 +4,16 @@ from . import index, rawSql
 from . import blender
 from . import get_version, get_user, get_project
 from . import get_area, get_device, get_var
+from . import get_record
 
 def add_get(router:UrlDispatcher):
     index.add_get(router)
     rawSql.add_get(router)
     blender.add_get(router)
     get_version.add_get(router)
+    get_user.add_get(router)
     get_project.add_get(router)
     get_area.add_get(router)
     get_device.add_get(router)
     get_var.add_get(router)
-    get_user.add_get(router)
+    get_record.add_get(router)
